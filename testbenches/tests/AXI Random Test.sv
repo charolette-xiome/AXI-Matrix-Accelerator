afe62399d57309b3cb949d473d23bc9823da2310
Utpal Raj Ambastha <167672969+charolette-xiome@users.noreply.github.com>
2026-09-24 00:52:36 +0530

    Create AXI Random Test.sv

---
+++ testbenches/tests/AXI Random Test.sv
@@ -0,0 +1,29 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class axi_rand_test extends uvm_test;
+    `uvm_component_utils(axi_rand_test)
+
+    axi_env env;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        env = axi_env::type_id::create("env", this);
+    endfunction
+
+    task run_phase(uvm_phase phase);
+        axi_rand_seq   seq;
+        
+        phase.raise_objection(this);
+
+        seq = axi_rand_seq::type_id::create("seq");
+        seq.start(env.axi_active.seqr);
+        
+        phase.drop_objection(this);
+    endtask
+
+endclass
